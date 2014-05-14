ffd5e19277456ff4f511aff6abf3f0e18271e0cf
DanielSprockett <danielsprockett@gmail.com>
2014-05-13 22:54:12 -0700

    Dan's PCA and HierClust

--- PCA_and_HierarchicalClustering.m
+++ PCA_and_HierarchicalClustering.m
@@ -54,31 +54,51 @@ for i = 1:number_of_samples;
             sum(diff_avg(:,i).*diff_avg(:,j));
     end
 end
+figure
 imagesc(covariance_matrix);
 colorbar;
-% disp(covariance_matrix);
 
 %%
-% step 4: calculate eigs of covariance_matrixariance matrix
-[V,D] = eigs(covariance_matrix);
-% disp(V);
-% disp(diag(D));
+% step 4: calculate eigs of covariance_matrix
+
+[eigenvectors,eigenvalues,explained] = pcacov(covariance_matrix);
+
+
+%% 
+% step 5: generate files for a PCA plot
+
+eigenvector_matrix = eigenvectors;
+add_labels = {'eigvals'; '% variation explained'};
+add_title = {'pc vector number'};
+pc_vector_labels = all_labels';
+pc_vector_number = vertcat(add_title,pc_vector_labels, add_labels);
+
+pc_labels = (1:size(all_labels,2));
+eigvals = eigenvalues';
+variation_explained = explained';
+
+eigenvector_matrix_labeled = vertcat(pc_labels, eigenvector_matrix, eigvals, variation_explained);
+eigenvector_table = table(pc_vector_number, eigenvector_matrix_labeled);
+
+writetable(eigenvector_table,'QIIME_proteomics_pc.txt','Delimiter','\t');
+
+% Then open in microsoft excel and delete the first line.
 
 %%
 % plot data with the eigenvectors 
 figure;
 scatter3(all_samples(:,1),all_samples(:,2),...
     all_samples(:,3),100,[1 0 0],'filled');
-ev1 = V(:,1)*D(1,1);
-ev2 = V(:,2)*D(2,2);
-ev3 = V(:,3)*D(3,3);
+ev1 = eigenvectors(:,1)*eigenvalues(1,1);
+ev2 = eigenvectors(:,2)*eigenvalues(2,2);
+ev3 = eigenvectors(:,3)*eigenvalues(3,3);
 hold on;
-plot3([avg(1)-ev1(1) avg(1)+ev1(1)],[avg(2)-ev1(2) avg(2)+ev1(2)], ...
-     [avg(3)-ev1(3) avg(3)+ev1(3)],'Color',[0 0 1],'LineWidth',3);
-plot3([avg(1)-ev2(1) avg(1)+ev2(1)],[avg(2)-ev2(2) avg(2)+ev2(2)], ...
-     [avg(3)-ev2(3) avg(3)+ev2(3)],'Color',[0 0 0],'LineWidth',3);
-plot3([avg(1)-ev3(1) avg(1)+ev3(1)],[avg(2)-ev3(2) avg(2)+ev3(2)], ...
-     [avg(3)-ev3(3) avg(3)+ev3(3)],'Color',[0 1 1],'LineWidth',3);
+%plot3([avg(1)-ev1(1) avg(1)+ev1(1)],[avg(2)-ev1(2) avg(2)+ev1(2)], ...
+ %    [avg(3)-ev1(3) avg(3)+ev1(3)],'Color',[0 0 1],'LineWidth',3);
+% plot3([avg(1)-ev2(1) avg(1)+ev2(1)],[avg(2)-ev2(2) avg(2)+ev2(2)], ...
+%     [avg(3)-ev2(3) avg(3)+ev2(3)],'Color',[0 0 0],'LineWidth',3);
+%plot3([avg(1)-ev3(1) avg(1)+ev3(1)],[avg(2)-ev3(2) avg(2)+ev3(2)], ...
+ %    [avg(3)-ev3(3) avg(3)+ev3(3)],'Color',[0 1 1],'LineWidth',3);
 hold off;
 axis equal
 
@@ -100,7 +120,7 @@ axis equal
 % This creates a clustergram of the data where proteins are on the y axis
 % and the samples are on the x axis. The goal is to group samples into
 % individual clusters. 
-clustergram(all_samples, 'RowLabels', proteins, 'ColumnLabels', all_labels', 'DisplayRange', 0.0005);
+clustergram(all_samples, 'RowLabels', proteins, 'ColumnLabels', all_labels', 'DisplayRange', 0.0005, 'colormap', 'winter');
 
 % clustergram(all_samples, 'RowPDist', 'spearman');
 %%
