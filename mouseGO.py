1280ebf37b72c15ec21b4fce72b7cb6f651e7179
evmasuta <evmasuta@gmail.com>
2014-05-17 17:46:47 -0700

    GO code Array
    Generates an array GOArray which has 3 columns and N rows, where N is
    equal to the number of proteins in OverlordMatrix (lacking decoy
    proteins).  Column 1 are the string GO codes, column 2 are the GO code
    types in order (biological, molecular, cellular), and column 3 are the
    numerical forms of the GO codes in column 1.  The indeces of the GOArray
    correspond to those in 'axes' from OverlordMatrix, i.e. row 2 of GOArray
    corresponds to protein 2.  This means that there are some empty rows in
    GOArray for proteins lacking GO codes, e.g. protein 1.

---
+++ mouseGO.py
@@ -0,0 +1,48 @@
+# This Python script reads in the information from the goa file for mouse.
+# The script then parses for only the proteins of interest (those in OverlordMatrix)
+# and saves the GO codes and types (biological, molecular, cellular) to file.
+import csv
+# http://bmi214.stanford.edu/index.php?n=Site.PythonTutorial#toc18
+# http://stackoverflow.com/questions/7856296/parsing-csv-tab-delimited-txt-file-with-python
+allData = list(csv.reader(open('gene_association.goa_mouse','rb'),delimiter='\t'))
+allData = allData[9:]
+counter = 0
+# GO code
+value1 = []
+# Biological, molecular, cellular
+value2 = []
+value = []
+dataDic = {}
+currentKey = allData[0][1]
+for i in xrange(len(allData)):
+    currentUniProt = allData[i][1]
+    if currentUniProt == currentKey:
+        if (len(value1) >= 1 and value1[len(value1)-1] != allData[i][4]):
+            value1.append(allData[i][4])
+            value2.append(allData[i][8])
+    else:
+        value = [value1,value2]
+        dataDic[allData[i-1][1]] = value
+        value = []
+        value1 = []
+        value2 = []
+        currentKey = allData[i][1]
+        value1.append(allData[i][4])
+        value2.append(allData[i][8])
+dataDic[currentUniProt] = value
+axes = list(csv.reader(open('axes.txt','rb'),delimiter='\t'));
+finalDic = {}
+f = open('final_Dictionary_GO.txt','w')
+for i in xrange(len(axes[0])):
+    currentProtein = axes[0][i][1:7]
+    if dataDic.has_key(currentProtein):
+        # Adjust for MATLAB 1 indexing
+        f.write(str(i+1) + '\n')
+        f.write(str(dataDic[currentProtein][0]) + '\n')
+        f.write(str(dataDic[currentProtein][1]) + '\n')
+        f.write('ENDLINE' + '\n')
+f.close()
+
+
+    
+    
