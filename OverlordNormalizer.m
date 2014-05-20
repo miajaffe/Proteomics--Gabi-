1ea9fc3ed8b781420625b14e91ff3b13dcbfa556
evmasuta <evmasuta@gmail.com>
2014-05-20 14:49:25 -0700

    New normalizer v1.0

--- OverlordNormalizer.m
+++ OverlordNormalizer.m
@@ -1,10 +1,9 @@
-% Feed in either 1 or 2  ato output normOverlord.  If
-% input is 1, the output is the OverlordMatrix
-% normalized by columns (down every row), i.e. comparisons can be made
-% for relative abundances of proteins in a given sample, e.g. we can
-% "compare" protein cadherin-1 to protein IL-2 within sample Peptide A.  If
-% the input is 2, the output is the OverlordMatrix
-% normalized by rows (across every column), i.e. comparisons can be made
+% Outputs the normalized OverlordMatrix normalized both across rows and
+% across columns.  Normalization by columns (down every row) allows
+% comparisons between relative abundance of proteins in a given sample,
+% e.g. we can compare protein cadherin-1 to protein IL-2 within sample
+% Peptide A.
+% Normalization by rows (across every column), i.e. comparisons can be made
 % for the relative abundance of a given protein across all samples, e.g. we
 % can "compare" protein cadherin-1's relative abundance across all samples
 % (Peptides A to ZZ).  Also required is the MOUSE.fasta downloaded from
@@ -14,13 +13,14 @@
 % UPDATE: If you have MusProt.mat in the working directory, the FASTA data
 % is not needed and the program runs fastA.
 
+
 % [redunancies] = the vector of indeces within axes for redundant protein
 % ID's.
 
 % Form to use if MusProtRaw.mat unavailable
 % function [normOverlord] = OverlordNormalizer(choice,MusProtRaw)
 % Form to use if MusProtRaw.mat is available
-function [normOverlord,redundancies] = OverlordNormalizer(choice)
+function [normOverlord,redundancies] = OverlordNormalizer()
 % Normalized by columns, compare all proteins within one sample.  To do
 % this, we must divide each protein's number of counts by the protein amino
 % acid length, since number of counts scales with number of enzymatic
@@ -35,77 +35,71 @@ function [normOverlord,redundancies] = OverlordNormalizer(choice)
 load('MusProt.mat')
 load('OverlordMatrix.mat');
 load('axes.mat');
-
-if choice == 1
-    % First step is to acquire the MW of each and every protein.
-    proteinID = {MusProt.Header};
-    proteinSeq = {MusProt.Sequence};
-    % Key is protein ID, value is length of protein in # amino acids
-    lengthMap = containers.Map();
-    for ii = 1:1:size(proteinID,2)
-        temp = proteinID{ii};
-        temp = temp(4:9);
-        lengthSeq = length(proteinSeq{1,ii});
-        lengthMap(temp) = lengthSeq;
-    end
-    colNormFactor = zeros([1 size(axes{1,1},2)]);
-    keysProt = axes{1,1};
-    noGood = {};
-    ggCounter = 0;
-    redundancies = [];
-    for ii = 1:1:length(colNormFactor)
-%         fprintf('%d\n',ii)
-        % Ignores false positives with '_' in ID name
-        if length(strfind(keysProt{ii}, '_')) == 0
-            if isKey(lengthMap,keysProt{ii}) == 1
-                colNormFactor(ii) = lengthMap(keysProt{ii});
-            else
-                % To deal with weird reduncancies, search the web to find
-                % AA length of equivalent protein
-                % http://stackoverflow.com/questions/8061344/how-to-search-for-a-string-in-cell-array-in-matlab
-                redundancies(length(redundancies)+1) = find(ismember(axes{1},keysProt{ii}));
-                url = strcat('http://www.uniprot.org/uniprot/',keysProt{ii});
-                urldata = urlread(url);
-                urlposition = findstr(urldata,' AA');
-                strcount = urlposition-1;
-                while isstrprop(urldata(strcount), 'digit') == 1
-                    strcount = strcount - 1;
-                end
-                templength = str2num(urldata(strcount+1:urlposition));
-                if length(urlposition) > 0
-                    colNormFactor(ii) = templength;
-                    % To deal with deleted UniProt entries, e.g.D3YUL9
-                else
-                    colNormFactor(ii) = 1;
-                end
-            end
-        else
-            % Prevent div by 0 downstream
-            colNormFactor(ii) = 1;
-        end
-    end
-    % Tile the colNormFacx
-    tiledNorm = repmat(colNormFactor',1,size(OverlordMatrix,2),size(OverlordMatrix,3),size(OverlordMatrix,4));
-    normOverlord = OverlordMatrix ./ tiledNorm;
-    % Normalized by rows, compare a given protein within all samples.  To do
-    % this, we must divide every protein's number of counts by the total number
-    % of counts within= the given sample.  This is because, even though the
-    % instrument and methods are generally the same, small differences affect
-    % the overall responsiveness to a given sample.
-    normOverlord1 = normOverlord;
-    save('normOverlord1','normOverlord1');
-elseif choice == 2
-    redundancies = [];
-    % Find the sum of all counts in every sample
-    rowNormFactor = sum(OverlordMatrix,1);
-    % Tile the rowNormFactor so that it has the same dimensions as
-    % OverlordMatrix
-    tiledNorm = repmat(rowNormFactor,size(OverlordMatrix,1),1,1,1);
-    % Divide OverlordMatrix by the tiledNorm
-    normOverlord = OverlordMatrix ./ tiledNorm;
-    normOverlord2 = normOverlord;
-    save('normOverlord2','normOverlord2')
-else
-    fprintf('Not a valid choice. GAME OVER. Insert 1 credit to continue.  Yes/No?')
+% First step is to acquire the MW of each and every protein.
+proteinID = {MusProt.Header};
+proteinSeq = {MusProt.Sequence};
+% Key is protein ID, value is length of protein in # amino acids
+lengthMap = containers.Map();
+for ii = 1:1:size(proteinID,2)
+    temp = proteinID{ii};
+    temp = temp(4:9);
+    lengthSeq = length(proteinSeq{1,ii});
+    lengthMap(temp) = lengthSeq;
 end
+colNormFactor = zeros([1 size(axes{1,1},2)]);
+keysProt = axes{1,1};
+noGood = {};
+ggCounter = 0;
+redundancies = [];
+for ii = 1:1:length(colNormFactor)
+    %         fprintf('%d\n',ii)
+    % Ignores false positives with '_' in ID name
+    if length(strfind(keysProt{ii}, '_')) == 0
+        if isKey(lengthMap,keysProt{ii}) == 1
+            colNormFactor(ii) = lengthMap(keysProt{ii});
+        else
+            % To deal with weird reduncancies, search the web to find
+            % AA length of equivalent protein
+            % http://stackoverflow.com/questions/8061344/how-to-search-for-a-string-in-cell-array-in-matlab
+            redundancies(length(redundancies)+1) = find(ismember(axes{1},keysProt{ii}));
+            url = strcat('http://www.uniprot.org/uniprot/',keysProt{ii});
+            urldata = urlread(url);
+            urlposition = findstr(urldata,' AA');
+            strcount = urlposition-1;
+            while isstrprop(urldata(strcount), 'digit') == 1
+                strcount = strcount - 1;
+            end
+            templength = str2num(urldata(strcount+1:urlposition));
+            if length(urlposition) > 0
+                colNormFactor(ii) = templength;
+                % To deal with deleted UniProt entries, e.g.D3YUL9
+            else
+                colNormFactor(ii) = 1;
+            end
+        end
+    else
+        % Prevent div by 0 downstream
+        colNormFactor(ii) = 1;
+    end
+end
+% Tile the colNormFacx
+tiledNorm = repmat(colNormFactor',1,size(OverlordMatrix,2),size(OverlordMatrix,3),size(OverlordMatrix,4));
+normOverlord = OverlordMatrix ./ tiledNorm;
+% Normalized by rows, compare a given protein within all samples.  To do
+% this, we must divide every protein's number of counts by the total number
+% of counts within= the given sample.  This is because, even though the
+% instrument and methods are generally the same, small differences affect
+% the overall responsiveness to a given sample.
+
+
+% Normalize across samples
+% Find the sum of all counts in every sample
+rowNormFactor = sum(normOverlord,1);
+% Tile the rowNormFactor so that it has the same dimensions as
+% normOverlord
+tiledNorm = repmat(rowNormFactor,size(normOverlord,1),1,1,1);
+% Divide normOverlord by the tiledNorm
+normOverlordF = normOverlord ./ tiledNorm;
+save('normOverlordF','normOverlordF')
+
 end
